0f88b8993bca1f693da7e6e30aa2cbfb62e2d486
tonykung <tonykung06@gmail.com>
2016-06-06 00:47:51 +0800

    initial commit

---
+++ mvc03/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc03.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "cannot be Empty")]
+        public string Code { get; set; }
+
+        [Display(Name = "Course Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Tutor { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ mvc03/Models/CourseDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc03.Models
+{
+    public class CourseDBContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CourseDBContext() : base("name=CourseDBContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<mvc03.Models.Course> Courses { get; set; }
+    
+    }
+}
